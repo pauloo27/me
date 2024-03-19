00c1d864d6a83134eb7894a186b22ad01cfbd0b2
Paulo <git@pauloo27.dev>
2024-03-19 20:48:03 -0300

    fix: 404 page

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ FROM code.db.cafe/pauloo27/garcom:latest
 WORKDIR /app
 COPY --from=builder /app/build .
 EXPOSE 80
-ENTRYPOINT ["garcom", "-error-404", "/app/404/index.html"]
+ENTRYPOINT ["garcom", "-clean-urls", "-error-404", "/app/404/index.html"]
--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -4,20 +4,9 @@
 
 <style>
 	main {
-		height: 100vh;
-		width: 100vw;
-
-		background-color: #212121;
-		text-align: center;
-
 		padding: 100px;
-
-		gap: 20px;
-
 		display: flex;
-		flex-direction: column;
 		justify-content: center;
-		align-items: center;
 	}
 
 	h2 {
