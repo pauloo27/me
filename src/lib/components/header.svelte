62e4e8133ecec9adef2d67e39e0ab5484d04d26d
Paulo <git@pauloo27.dev>
2024-03-21 15:37:02 -0300

    feat: add responsive nav menu
    with js tho :(

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,17 +1,60 @@
 <script>
 	import { toggleTheme } from '$lib/theme';
+
+	const links = [
+		{
+			name: 'Home',
+			path: '/'
+		},
+		{
+			name: 'Blog',
+			path: 'https://faze.ndo.cafe',
+			extern: true
+		},
+		{
+			name: 'Linkedin',
+			path: 'https://www.linkedin.com/in/paulofaversani/',
+			extern: true
+		},
+		{
+			name: 'Github',
+			path: 'https://github.com/pauloo27',
+			extern: true
+		}
+	];
+
+	let isMenuOpen = false;
 </script>
 
-<header>
+<header id="full">
 	<nav>
-		<a href="/">Home</a>
-		<a rel="noopener noreferrer" target="_blank" href="https://faze.ndo.cafe">Blog</a>
-		<a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/paulofaversani/">
-			Linkedin
-		</a>
-		<a rel="noopener noreferrer" target="_blank" href="https://github.com/pauloo27">Github</a>
+		{#each links as link}
+			{#if link.extern}
+				<a rel="noopener noreferrer" target="_blank" href={link.path}>{link.name}</a>
+			{:else}
+				<a href={link.path}>{link.name}</a>
+			{/if}
+		{/each}
 	</nav>
 	<button on:click={toggleTheme}><i class="bx bx-bulb" /></button>
+</header>
+
+<header id="minimal">
+	<div id="minimal-buttons">
+		<button on:click={() => (isMenuOpen = !isMenuOpen)}><i class="bx bx-menu" /></button>
+		<button on:click={toggleTheme}><i class="bx bx-bulb" /></button>
+	</div>
+	{#if isMenuOpen}
+		<nav>
+			{#each links as link}
+				{#if link.extern}
+					<a rel="noopener noreferrer" target="_blank" href={link.path}>{link.name}</a>
+				{:else}
+					<a href={link.path}>{link.name}</a>
+				{/if}
+			{/each}
+		</nav>
+	{/if}
 </header>
 
 <style>
@@ -22,14 +65,40 @@
 		justify-content: space-between;
 	}
 
+	#minimal {
+		display: none;
+		padding: 10px 20px;
+		flex-direction: column;
+	}
+
+	#minimal-buttons {
+		display: flex;
+		justify-content: space-between;
+	}
+
 	nav {
 		display: flex;
 		gap: 20px;
+	}
+
+	#minimal > nav {
+		flex-wrap: wrap;
+		margin: 5px 0;
 	}
 
 	a {
 		color: var(--foreground);
 		text-decoration: none;
 		font-weight: 500;
+	}
+
+	@media (max-width: 768px) {
+		#minimal {
+			display: flex;
+		}
+
+		#full {
+			display: none;
+		}
 	}
 </style>
